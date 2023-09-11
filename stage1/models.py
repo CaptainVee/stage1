cf26ffa93b90eb68a9030f7c3b2537bdea7f0c21
CaptainVee <captainvee3@gmail.com>
2023-09-11 12:19:16 +0100

    added validation

--- stage1/models.py
+++ stage1/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_string(value):
+    if not isinstance(value, str):
+        raise ValidationError("Only strings are allowed for this field.")
+
 
 class Person(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, validators=validate_string)
